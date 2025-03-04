00eda64fd88ed9353e7994c9e3242c183bc13792
{your_name} <mouhamed.taher.souguir.work@gmail.com>
2025-03-05 00:09:55 +0100

    empty

--- greenify/greenify_front/lib/utils/helpers.dart
+++ greenify/greenify_front/lib/utils/helpers.dart
@@ -2,26 +2,4 @@ class Helpers {
   static bool isNameValid(String name) {
     return RegExp(r'^[a-z A-Z\s]+$').hasMatch(name);
   }
-  static bool isForeNameValid(String foreName){
-    return RegExp(r'^[a-z A-Z\s]+$').hasMatch(foreName);
-  }
-  static bool isEmailValid(String email) {
-    return RegExp(r'^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+').hasMatch(email);
-  }
-  static bool isPhoneNumberValid(String phoneNumber) {
-    return RegExp(r'^[0-9]{8}$').hasMatch(phoneNumber);
-  }
-  static bool hasPasswordMinimumLength(String password) {
-    return password.length >= 8;
-  }
-  static bool hasPasswordUpperCase(String password) {
-    return RegExp(r'[A-Z]').hasMatch(password);
-  }
-  static bool hasPasswordLowerCase(String password) {
-    return RegExp(r'[a-z]').hasMatch(password);
-  }
-  static bool hasPasswordDigit(String password) {
-    return RegExp(r'[0-9]').hasMatch(password);
-  }
-  
 }
