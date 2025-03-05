c173b2ba3e3392619e3d797d6596687d65e412c8
{your_name} <mouhamed.taher.souguir.work@gmail.com>
2025-03-05 01:15:54 +0100

    empty

--- greenify/greenify_front/lib/utils/constants.dart
+++ greenify/greenify_front/lib/utils/constants.dart
@@ -1,6 +1,12 @@
-class Constants {
+import 'dart:ui';
+
+class appConstants {
   static const String appName = 'Greenify';
   static const String sendGridApiKey =
       "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";
   static const String apiUrl = "http://10.0.2.2:5000/";
+}
+
+class appColors {
+  static const Color greenifyGreen = Color(0xFF148C20);
 }
